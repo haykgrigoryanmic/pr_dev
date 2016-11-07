4af3ef29e978cba38ea5eb5dcc068bb2ded43de5
Hayk Grigoryan <hayk.g.grigoryan@gmail.com>
2016-11-07 21:20:12 +0400

    987d'fd

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,7 +5,7 @@ class Welcome extends CI_Controller {
 
 
 
-	
+
 	/**
 	 * Index Page for this controller.
 	 *
@@ -16,7 +16,7 @@ class Welcome extends CI_Controller {
 	 *	- or -
 	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
-	 *
+	 *kx.kc
 	 * So any other public methods not prefixed with an underscore will
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
